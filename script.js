3923a973b49223c11d5a76ca66427088d8a29f38
Max Base <MaxBaseCode@Gmail.Com>
2019-05-12 16:59:06 +0430

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+/**
+*
+* @Name : script.js
+* @Version : 1.0
+* @Programmer : Max
+* @Date : 2019-05-12
+* @Released under : https://github.com/BaseMax/ChessWeb/blob/master/LICENSE
+* @Repository : https://github.com/BaseMax/ChessWeb
+*
+**/
 let table=document.querySelector("#table");
 let pieces=document.querySelectorAll(".piece");
 console.log(pieces);
