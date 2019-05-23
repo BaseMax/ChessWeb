bd478853dca35f13ce4e3a6b45475fdb7aaab7b8
Max Base <MaxBaseCode@Gmail.Com>
2019-05-23 17:47:25 +0430

    Update script.js

--- script.js
+++ script.js
@@ -18,6 +18,22 @@ function get(x, y) {
 	return pieces[(x-1) * 8 + (y-1)]
 }
 
+function getByIndex(index) {
+	return pieces[index]
+}
+
+function parseIndex(index) {
+	let row = Math.floor(index / 8) + 1
+	let column=index - ((row-1) * 8) + 1
+	// console.log(row)
+	// console.log(column)
+	return {row: row, column: column}
+}
+
+function getIndex(x, y) {
+	return (x-1) * 8 + (y-1)
+}
+
 function typeMe() {
 	return timeMe === true ? "black" : "white"
 	// return pieceMe === true ? "black" : "white"
@@ -77,10 +93,11 @@ function getChildNumber(node) {
 
 // function offerPiece(piece, pieceIndex, array, row, column) {
 function offerPiece(piece, array, row, column) {
-	let pieceIndex=row*8 + column
-	pieceIndex++
-	row++
-	column++
+	// let pieceIndex=row*8 + column
+	// pieceIndex++
+	// row++
+	// column++
+	let pieceIndex=(row-1)*8 + (column-1)
 	console.log("pieceIndex : " + pieceIndex)
 	console.log("row : " + row)
 	console.log("column : " + column)
@@ -140,23 +157,72 @@ pieces.forEach(function(piece, index, array) {
 		// pieces.forEach(function(_piece, _index, _array) {
 		let parent=piece.parentElement
 		let parentClasses=parent.classList
-		if(parentClasses.contains("cango")) {
+		let parentParent=parent.parentElement
 
+		let row=getChildNumber(parentParent) + 1
+		let column=getChildNumber(parent) + 1
+
+		if(parentClasses.contains("cango")) {
+			let currentPiece
+			let currentRow
+			let currentColumn
+			pieces.forEach(function(_piece, _index) {
+				let _parent=_piece.parentElement
+				let _parentClasses=_parent.classList
+				_parentClasses.remove("cango");
+				if(_parentClasses.contains("selected")) {
+					_parentClasses.remove("selected")
+					currentPiece=_piece
+					let _position=parseIndex(_index)
+					currentRow=_position.row
+					currentColumn=_position.column
+				}
+			})
+			// console.log(currentPiece)
+			// console.log(currentRow)
+			// console.log(currentColumn)
+			// console.log(getIndex(row, column))
+
+			// pieces[getIndex(row, column)]=currentPiece
+			// newPiece=currentPiece
+			let newPiece=pieces[getIndex(row, column)]
+			newPieceParent=newPiece.parentNode
+			newPieceParentClasses=newPiece.parentNode.classList
+			if(currentPiece.classList.contains("white")) {
+				newPiece.classList.add("white")
+				newPiece.classList.remove("black")
+			}
+			else if(currentPiece.classList.contains("black")) {
+				newPiece.classList.add("black")
+				newPiece.classList.remove("white")
+			}
+			newPiece.classList.add(typePiece(pieces[getIndex(currentRow, currentColumn)]))
+			console.log(getIndex(row, column))
+
+			// Types = rook, knight, king, queen, bishop, pawn
+			currentPiece.classList.remove("rook")
+			currentPiece.classList.remove("knight")
+			currentPiece.classList.remove("king")
+			currentPiece.classList.remove("queen")
+			currentPiece.classList.remove("bishop")
+			currentPiece.classList.remove("pawn")
+
+			// pieces[getIndex(currentRow, currentColumn)]=currentPiece
 		}
-		pieces.forEach(function(_piece) {
-			let _parent=_piece.parentElement
-			let _parentClasses=_parent.classList
-			_parentClasses.remove("selected")
-			_parentClasses.remove("cango")
-		})
-		if(piece.classList.contains(typeMe())) {
-			parentClasses.add("selected")
-			let row=getChildNumber(piece.parentElement.parentElement)
-			let column=getChildNumber(piece.parentElement)
-			// console.log(getChildNumber(piece.parentElement))
-			// console.log(getChildNumber(piece.parentElement.parentElement))
-			// offerPiece(piece, index, array, row, column)
-			offerPiece(piece, array, row, column)
+		else {
+			pieces.forEach(function(_piece) {
+				let _parent=_piece.parentElement
+				let _parentClasses=_parent.classList
+				_parentClasses.remove("selected")
+				_parentClasses.remove("cango")
+			})
+			if(piece.classList.contains(typeMe())) {
+				parentClasses.add("selected")
+				// console.log(getChildNumber(piece.parentElement))
+				// console.log(getChildNumber(piece.parentElement.parentElement))
+				// offerPiece(piece, index, array, row, column)
+				offerPiece(piece, array, row, column)
+			}
 		}
 	})
 })
